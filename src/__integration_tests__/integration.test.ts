7566bb0de33ab9b1136b7a4d6356fea7c6c7b824
Grant Forrest <gaforres@gmail.com>
2019-05-25 12:21:55 -0400

    add integration test for lists

--- src/__integration_tests__/integration.test.ts
+++ src/__integration_tests__/integration.test.ts
@@ -62,14 +62,14 @@ describe('read queries', () => {
 
     expect(errors).toBeUndefined();
     expect(data).toMatchInlineSnapshot(`
-            Object {
-              "person": Object {
-                "age": 52,
-                "firstName": "Hans",
-                "id": "e32ae442-f5cc-4b4e-b440-e385d5e15d57",
-              },
-            }
-        `);
+                  Object {
+                    "person": Object {
+                      "age": 52,
+                      "firstName": "Hans",
+                      "id": "e32ae442-f5cc-4b4e-b440-e385d5e15d57",
+                    },
+                  }
+            `);
   });
 
   test('a nested in-graph query', async () => {
@@ -100,14 +100,61 @@ describe('read queries', () => {
 
     expect(errors).toBeUndefined();
     expect(data).toMatchInlineSnapshot(`
+            Object {
+              "person": Object {
+                "firstName": "Hans",
+                "id": "e32ae442-f5cc-4b4e-b440-e385d5e15d57",
+                "livesIn": Object {
+                  "id": "ca5750a6-1df3-4e21-8321-a41ee3b9df98",
+                  "name": "Bahrain",
+                },
+              },
+            }
+        `);
+  });
+
+  test('a nested list in-graph query', async () => {
+    const personId = people[0].id;
+    const query = `
+      query NestedQuery($id: ID!) {
+        person(id: $id) {
+          id
+          firstName
+          skills {
+            id
+            name
+          }
+        }
+      }
+    `;
+
+    const { data, errors } = await graphql({
+      schema,
+      source: query,
+      variableValues: {
+        id: personId,
+      },
+      contextValue: {
+        neo4jDriver: driver,
+      },
+    });
+
+    expect(errors).toBeUndefined();
+    expect(data).toMatchInlineSnapshot(`
       Object {
         "person": Object {
           "firstName": "Hans",
           "id": "e32ae442-f5cc-4b4e-b440-e385d5e15d57",
-          "livesIn": Object {
-            "id": "ca5750a6-1df3-4e21-8321-a41ee3b9df98",
-            "name": "Bahrain",
-          },
+          "skills": Array [
+            Object {
+              "id": "c0b52e9a-e762-4148-a407-db425476a0c2",
+              "name": "parsing",
+            },
+            Object {
+              "id": "d5313797-8d6b-4fa6-bcfd-4574a83b056c",
+              "name": "connecting",
+            },
+          ],
         },
       }
     `);
